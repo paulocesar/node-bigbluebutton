d840504d46fee1134e0368feff8721923813018a
Paulo Cesar <pauloc062@gmail.com>
2013-07-13 16:03:38 -0300

    adding get and post methods

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 var bbb = module.exports = require('./lib/bigbluebutton');
 
-bbb.salt = '639259d4-9dd8-4b25-bf01-95f9567eaf4b';
-link = bbb.link('create',{
-  'name': 'Test Meeting',
-  'meetingID': 'abc123',
-  'attendeePW': '111222',
-  'moderatorPW': '333444'
-});
+// bbb.salt = '639259d4-9dd8-4b25-bf01-95f9567eaf4b';
+// bbb.url = 'http://www.reddit.com/';
 
-console.log(link);
+// link = bbb.link('create',{
+//   'name': 'Test Meeting',
+//   'meetingID': 'abc123',
+//   'attendeePW': '111222',
+//   'moderatorPW': '333444'
+// });
+// console.log(link + "\n");
+
+// bbb.get('search/.xml',{
+//   q: '10'
+// },function (response){
+//   console.log(response + "\n");
+// });
+
+// bbb.post('search/.xml'
+//   ,{q: '10'}
+//   ,{exemplo:{hi:'hello',content:{sample:'content'}}}
+//   ,function (response){
+//     console.log(response + "\n");
+//   }
+// );
+
+// https://npmjs.org/package/xml2json
+// https://github.com/buglabs/node-xml2json
+// http://docs.nodejitsu.com/articles/HTTP/clients/how-to-create-a-HTTP-request
