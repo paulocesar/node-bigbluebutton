7325f3d00f8265f6dac769ed69a18ab613c17fb6
Paulo Cesar <pauloc062@gmail.com>
2013-07-14 10:52:27 -0300

    passing parameters as object and adding input validation

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 var bbb = module.exports = require('./lib/bigbluebutton');
 
-// bbb.salt = '639259d4-9dd8-4b25-bf01-95f9567eaf4b';
-// bbb.url = 'http://www.reddit.com/';
+bbb.salt = '639259d4-9dd8-4b25-bf01-95f9567eaf4b';
+bbb.url = 'http://www.reddit.com';
 
-// link = bbb.link('create',{
-//   'name': 'Test Meeting',
-//   'meetingID': 'abc123',
-//   'attendeePW': '111222',
-//   'moderatorPW': '333444'
-// });
+// data = {
+//   action: 'create',
+//   params: {
+//     name: 'Test Meeting',
+//     meetingID: 'abc123',
+//     attendeePW: '111222',
+//     moderatorPW: '333444'
+//   }
+// }
+// link = bbb.link(data);
 // console.log(link + "\n");
 
-// bbb.get('search/.xml',{
-//   q: '10'
-// },function (response){
-//   console.log(response + "\n");
+// data = {
+//   action: 'search/.xml',
+//   params: { q: 10 }
+// }
+// bbb.get(data,function (response){
+//   console.log(bbb.toJson(response));
 // });
 
-// bbb.post('search/.xml'
-//   ,{q: '10'}
-//   ,{exemplo:{hi:'hello',content:{sample:'content'}}}
-//   ,function (response){
-//     console.log(response + "\n");
-//   }
-// );
+data = {
+  action: 'search.xml',
+  params: { q: 10 },
+  body: {
+    modules: {
+      module: [
+        {
+          name:'presentation',
+          document:{url:'example.pdf'}
+        }
+      ]
+    }
+  }
+}
+bbb.post(data,function (response){
+  console.log(response + "\n");
+});
 
 // https://npmjs.org/package/xml2json
 // https://github.com/buglabs/node-xml2json
