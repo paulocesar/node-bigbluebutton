c04355c88a2996cd48224d4dee2f198c1e27f348
Paulo Cesar <pauloc062@gmail.com>
2013-08-27 13:24:55 -0300

    testing action validation

--- test/test-validation.js
+++ test/test-validation.js
@@ -9,12 +9,36 @@ describe("validation", function (){
 
   // TODO
   describe("data",function () {
-    it("should throw a error on null data",function(done) {
+    it("should throw a error on null data",function (done) {
       data = null;
       validation(data,function(er){
         if(!er) throw Error("Should have an error");
+        done()
+      })
+    })
+
+  })
+
+  describe("action",function () {
+
+    it("should throw without an action",function (done) {
+      data = {}
+      validation(data,function(er){
+        if(!er) throw Error("Should have an error");
+        done()
+      })
+    })
+
+    it("should have only letters, numbers, _ and -",function (done) {
+      data = {action: "sample_23-1"};
+      validation(data,function(er){
+        if(er) throw er;
+      })
+      data = {action: "sample_23-1*&!@#"};
+      validation(data,function(er){
+        if(!er) throw Error("Should have an error");
         done();
-      });
+      })
     })
 
   })
